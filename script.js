52677a149ab5d2a876a20ff1d64d6d34f932575f
Haris Saeed <96650020+Quitzelcoat@users.noreply.github.com>
2023-10-31 11:58:20 +0500

    Button click show on input field.

--- script.js
+++ script.js
@@ -1,24 +1,14 @@
+const input = document.getElementById("input");
+const btn = document.querySelectorAll(".btn");
+
 let numOne = "";
 let numTwo = "";
 let operator = "";
 
-const add = function(a, b) {
-    return a + b;
-}
-
-const subtract = function(a, b) {
-    return a - b;
-}
-
-const multiply = function(a, b) {
-    return a * b;
-}
-
-const divide = function(a, b) {
-    return a / b;
-}
-
 function operate(num1, num2, operator) {
+    num1 = parseInt(num1);
+    num2 = parseInt(num2);
+
     switch(operator) {
         case '+':
             return add(num1, num2);
@@ -26,7 +16,26 @@ function operate(num1, num2, operator) {
             return subtract(num1, num2);
         case '*':
             return multiply(num1, num2);
-        case '/':
-            return divide(num1, num2);
+            case '/':
+                if (num2 === 0) {
+                    return "Error: Division by zero";
+                }
+                return divide(num1, num2);
+            default:
+                return "Error: Invalid operator";
     }
 }
+
+
+function displayShow() {
+
+    btn.forEach(btn => {
+        btn.addEventListener("click", function() {
+            let val = btn.value;
+            input.value += val;
+        });
+    });
+}
+
+displayShow();
+
